ecd285b8b4af3d2445fc72a46b77c7bf4924548d
soljung1997 <sol.jung1997@gmail.com>
2024-07-19 20:19:31 +0900

    6.0

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ window.onscroll = function() {
 
 function scrollFunction() {
     var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
-    if (scrollPosition > 650) {
+    if (scrollPosition > 900) {
         document.getElementById("navbar").classList.add("scrolled");
     } else {
         document.getElementById("navbar").classList.remove("scrolled");
@@ -24,16 +24,41 @@ function scrollFunction() {
 
 function scrollBodyColor() {
     var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
-
-    if (scrollPosition > 500) {
+    var gradient1 = document.querySelector('.photo-container-background');
+    var gradient2 = document.querySelector('.filler-4-gradient-2');
+    var gradient3 = document.querySelector(".photo-container-2-background");
+    var gradient4 = document.querySelector(".album-container-background");
+    var album2 = document.querySelector('.photo-individual-4');
+    var text = document.querySelector(".text-box");
+    var mainContainer = document.querySelector('.main-container-about');
+    var navbarGradient = document.querySelector('.navbar-gradient');
+    
+    if (scrollPosition > 900) {
+        if (gradient1) gradient1.style.opacity = 1;
+        if (gradient2) gradient2.style.opacity = 1;
+        if (gradient3) gradient3.style.opacity = 1;
+        if (gradient4) gradient4.style.opacity = 1;
+        if (album2) album2.style.opacity = 1;
         document.body.style.backgroundColor = "#fff";
-        document.body.style.color = "#fff"; /* White text */
+        if (mainContainer) mainContainer.style.backgroundColor = "#fff";
+        if (text) text.style.color = "#000";
+        if (navbarGradient) navbarGradient.style.opacity = 1;
     } else {
+        if (gradient1) gradient1.style.opacity = 0;
+        if (gradient2) gradient2.style.opacity = 0;
+        if (gradient3) gradient3.style.opacity = 0;
+        if (gradient4) gradient4.style.opacity = 0;
+        if (album2) album2.style.opacity = 0;
         document.body.style.backgroundColor = "#000"; /* Black */
-        document.body.style.color = "#fff"; /* White text */
-
+        if (mainContainer) mainContainer.style.backgroundColor = "#000";
+        if (text) text.style.color = "#fff";
+        if (navbarGradient) navbarGradient.style.opacity = 0;
     }
 }
+
+// Attach the function to the scroll event
+window.addEventListener('scroll', scrollBodyColor);
+
 
 function scrollOverlayTextColor() {
     var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
@@ -51,51 +76,36 @@ function scrollOverlayTextColor() {
 // Attach the function to the window's scroll event
 window.addEventListener('scroll', scrollOverlayTextColor);
 
-/*
-document.addEventListener('DOMContentLoaded', function() {
 
+document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
-        const navbarHeightVH = 10; // Adjust if there's a fixed navbar
-        const windowHeight = window.innerHeight;
-        const navbarHeight = (navbarHeightVH / 100) * windowHeight; // Convert 10vh to pixels
         const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        for(let i = 1; i <= 4; i++){
-            let elements = `.photo-individual-${i}`;
-            const photoContainers = document.querySelectorAll(elements);
-            photoContainers.forEach(elements => {
-                const photoImage = elements.querySelector('img');
-                const containerTop = elements.getBoundingClientRect().top + scrollPosition - navbarHeight;
-                const containerHeight = elements.offsetHeight;
-    
-                if (scrollPosition >= containerTop && scrollPosition - navbarHeight < containerTop + containerHeight) {
-                    console.log("Bottom");
-                    console.log("WH: " + windowHeight + "containerTop: " + containerTop + "navbar:" + navbarHeight);
-                    const visibleHeight = Math.max(0, containerHeight + navbarHeight - (scrollPosition - containerTop));
-                    photoImage.style.clipPath = `inset(0 0 ${containerHeight - visibleHeight + navbarHeight}px 0)`;
-                    photoImage.style.transform = `translateY(${scrollPosition - containerTop}px)`;
-                } else if (scrollPosition + windowHeight >= containerTop && scrollPosition + navbarHeight < containerTop) { //if scroll + height of VP less than container top, and scroll + navbar greater than container top
-                    const visibleHeight = Math.max(0, containerHeight - (containerTop + containerHeight - (scrollPosition + windowHeight)));
-                    console.log("Top");
-                    console.log("WH: " + windowHeight + " containerTop: " + containerTop + " navbar:" + navbarHeight);
-                    photoImage.style.clipPath = `inset(${containerHeight - visibleHeight + navbarHeight}px 0 0 0)`;
-                    photoImage.style.transform = `translateY(${scrollPosition - containerTop + navbarHeight}px)`;
-                } else {
-                    console.log("Else");
-                    console.log("WH: " + windowHeight + " containerTop: " + containerTop + " navbar:" + navbarHeight);
-                    photoImage.style.clipPath = 'inset(0 0 0 0)';
-                    photoImage.style.transform = 'translateY(0)';
-                }
-            });
-        }
     });
-});*/
+});
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    const img1 = document.querySelector('.photo-individual-1');
+    const img2 = document.querySelector('.photo-individual-2');
+    const img3 = document.querySelector('.photo-individual-3');
+    const img4 = document.querySelector('.photo-individual-4');
+    
+    // Store the original positions
+    const containerOriginalTop = {
+        img1: img1.getBoundingClientRect().top,
+        img2: img2.getBoundingClientRect().top,
+        img3: img3.getBoundingClientRect().top,
+        img4: img4.getBoundingClientRect().top
+    };
+
     window.addEventListener('scroll', function() {
+        //static variables
         const navbarHeightVH = 10; // Adjust if there's a fixed navbar
         const windowHeight = window.innerHeight;
         const navbarHeight = (navbarHeightVH / 100) * windowHeight; // Convert 10vh to pixels
+        const windowTop = navbarHeight;
+        const windowBottom = windowHeight;
+        //dynamic variables
         const scrollPosition = window.scrollY || document.documentElement.scrollTop;
         
         for (let i = 1; i <= 4; i++) {
@@ -105,53 +115,35 @@ document.addEventListener('DOMContentLoaded', function() {
             
             photoContainers.forEach(element => {
                 const photoImage = element.querySelector('img');
+                //dynamic variables
                 const containerTop = element.getBoundingClientRect().top;
                 const containerBottom = element.getBoundingClientRect().bottom;
                 const containerHeight = element.offsetHeight;
-                const padding = containerTop * 0.2;
+                const augmentedContainerHeight = containerHeight - windowHeight;
 
-                console.log(`Element: .photo-individual-${i}`);
-                console.log(`Container Top: ${containerTop}`);
-                console.log(`Container Height: ${containerHeight}`);
-                console.log(`Navbar Height: ${navbarHeight}`);
-                console.log("NH:" + (navbarHeight) + " CB: " + (containerBottom));
+                // Access the original position using the originalPos object
+                const originalPosition = containerOriginalTop[`img${i}`];
 
-
-                if (navbarHeight <= containerTop || containerBottom >= windowHeight) { //if top of container touches bottom of VP
-                    console.log("bottom");
-                    console.log("CB: " + containerBottom + " windowH: " + windowHeight);
-                    const visibleHeight = Math.max(0, + -((containerBottom - windowHeight) - containerHeight));
-                    console.log("CH: " +  containerHeight + " WH:" + (windowHeight) + " CB: " + (containerBottom));
-                    console.log(`Visible Height (Bottom): ${visibleHeight}`);
-                    const clip = photoImage.style.clipPath = `inset(${containerHeight - visibleHeight - navbarHeight}px 0 0 0)`; //has to be positive to 0
-                    console.log("clip:" + clip);
-                    const transform = photoImage.style.transform = `translateY(-${containerHeight - visibleHeight - navbarHeight}px)`; //should move with container height
-                    console.log("transform: " + transform);
-                } else if (navbarHeight >= containerTop && windowHeight >= containerBottom) { //if top of container touches top of VP & top of VP <= bottom of container
-                    console.log("Top");
-                    console.log("CT:" + (containerTop) + " NH: " + (navbarHeight) + "WH:" + (windowHeight) + " CB: " + (containerBottom));
-                    const visibleHeight = Math.max(0, containerBottom);
-                    console.log(`Visible Height (Top): ${visibleHeight}`);
-                    const clip = photoImage.style.clipPath = `inset(0 0 ${containerHeight - visibleHeight + navbarHeight}px 0)`;
-                    console.log("clip:" + clip);
-                    const transform = photoImage.style.transform = `translateY(${containerHeight - visibleHeight + navbarHeight}px)`;
-                    console.log("transform: " + transform);
-                } else {
-                    console.log("Else");
-                    photoImage.style.clipPath = `inset(0 0 0 0)`;
-                    const transform = photoImage.style.transform = `translateY(0px)`;
-                    console.log("transform: " + transform);
-                }
+                    //CB < WH && CT > NH - if bottom of container is lesser than original bottom (shouldnt it be greater?)
+                if (containerTop <= 0) { //if top of container touches bottom of VP
+                    //visibleHeight is the pixels height of the current container
+                    const visibleHeight = -(containerTop);
+                    if (containerHeight > windowHeight){
+                        //inset top right bottom left
+                        //has to inset containerheight(static) - visibleHeight(dynamic) - augmentedContainerHeight(static)
+                        const clip = photoImage.style.clipPath = `inset(0 0 ${(-containerTop) - augmentedContainerHeight}px 0)`; //has to be positive to 0
+                        const transform = photoImage.style.transform = `translateY(${-(containerTop) - augmentedContainerHeight}px)`; //should move with container height
+                    }else{
+                        //normal clip
+                        const clip = photoImage.style.clipPath = `inset(0 0 ${(-containerTop)}px 0)`; //has to be positive to 0
+                        const transform = photoImage.style.transform = `translateY(${-(containerTop)}px)`; //should move with container height
+                    }
+                } else if(containerTop <= windowBottom && containerBottom >= windowBottom){
+                    const visibleHeight = containerBottom - windowHeight; //will be a negative number, so convert
+                    const clip = photoImage.style.clipPath = `inset(${visibleHeight}px 0 0 0)`; //has to be positive to 0
+                    const transform = photoImage.style.transform = `translateY(${-(containerTop - windowHeight + containerHeight)}px)`; //transform bottom too, but with containerheight into consideration
+                } 
             });
         }
-    });
-});
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        console.log('Scroll Position:', scrollPosition);
     });
 });
