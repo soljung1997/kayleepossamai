80347dab6277550f43476b73aa1189d6d896c7bc
soljung1997 <sol.jung1997@gmail.com>
2024-08-16 15:04:47 +0900

    12.92

--- js/image_append.js
+++ js/image_append.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
                 // Function to initialize hover-based scrolling
                 function initializeHoverScrolling() {
                     const container = document.querySelector('#galleryContainer');
+                    const container_scroll = document.querySelector('#portfolioContainer');
 
                     container.addEventListener('mousemove', function(e) {
                         const rect = container.getBoundingClientRect();
@@ -43,12 +44,12 @@ $(document).ready(function() {
                         }
 
                         // Update scroll position
-                        container.scrollLeft -= scrollAmount; // Scroll left for mouse on left side, right for mouse on right side
+                        container_scroll.scrollLeft -= scrollAmount; // Scroll left for mouse on left side, right for mouse on right side
                     });
 
                     // Optional: Reset scroll position when mouse leaves
                     container.addEventListener('mouseleave', function() {
-                        container.scrollLeft = 0;
+                        container_scroll.scrollLeft = 0;
                     });
                 }
 
